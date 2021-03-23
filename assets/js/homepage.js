30e7437be0183b2930017d8ec1a196bdca4bfee9
michealjwellis <mellis.eriecon@gmail.com>
2021-03-23 12:05:09 -0400

    finished lesson 6.2.6 of module 6

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -23,14 +23,28 @@ var getUserRepos = function (user) {
 
   // make a request to the url
   fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-      displayRepos(data, user);
-      console.log(data);
-    })
+    if(response.ok) {
+        response.json().then(function (data) {
+        displayRepos(data, user);
+        console.log(data);
+      })
+    } else {
+      alert("Error: " + response.statusText);
+    } 
+    
+  })
+  .catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
   })
 }
 
 var displayRepos = function(repos, searchTerm) {
+  // check if api returned any repos 
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
   console.log(repos);
   console.log(searchTerm);
 
@@ -63,6 +77,9 @@ var displayRepos = function(repos, searchTerm) {
       statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
     }
     // append to container 
+    repoEl.appendChild(statusEl);
+
+    // append container to the dom
     repoContainerEl.appendChild(repoEl);
   }
 }
